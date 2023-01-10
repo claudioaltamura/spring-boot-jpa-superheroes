aedb4f825b77a53f79e0ffb0b34eea7ee792bd8e
Claudio Altamura <claudio@claudioaltamura.de>
2023-01-10 17:31:32 +0100

    new: added runner

--- src/main/java/de/claudioaltamura/springboot/jpa/SuperheroesApplication.java
+++ src/main/java/de/claudioaltamura/springboot/jpa/SuperheroesApplication.java
@@ -1,13 +1,31 @@
 package de.claudioaltamura.springboot.jpa;
 
+import de.claudioaltamura.springboot.jpa.superheroes.entity.CityEntity;
+import de.claudioaltamura.springboot.jpa.superheroes.entity.SuperheroEntity;
+import de.claudioaltamura.springboot.jpa.superheroes.repository.SuperheroEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SuperheroesApplication {
+
+	@Autowired
+	private SuperheroEntityRepository superheroEntityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			if(!superheroEntityRepository.existsById(1L)) {
+				final var city = new CityEntity("Gotham City");
+				superheroEntityRepository.save(new SuperheroEntity("Batman", "Bruce Wayne", 92.0d, city));
+			}
+		};
+	}
 }
