b0132c81c6089b3fef36e78b786de907fedf6e63
Claudio Altamura <claudio@claudioaltamura.de>
2023-06-08 18:18:40 +0200

    feat(queries): added query examples

--- src/main/java/de/claudioaltamura/springboot/jpa/superheroes/repository/SuperheroEntityRepository.java
+++ src/main/java/de/claudioaltamura/springboot/jpa/superheroes/repository/SuperheroEntityRepository.java
@@ -5,7 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.claudioaltamura.springboot.jpa.superheroes.entity.SuperheroEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SuperheroEntityRepository extends JpaRepository<SuperheroEntity, Long> {
 	List<SuperheroEntity> findByName(String name);
+
+	List<SuperheroEntity> findByNameStartsWith(String name);
+
+	@Query(value = "SELECT * FROM Superheroes s WHERE s.name LIKE %:name%", nativeQuery = true)
+	List<SuperheroEntity> searchContainsByNameLike(@Param("name") String name);
 }
--- src/test/java/de/claudioaltamura/springboot/jpa/superheroes/entity/SuperheroEntityRepositoryTest.java
+++ src/test/java/de/claudioaltamura/springboot/jpa/superheroes/entity/SuperheroEntityRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import de.claudioaltamura.springboot.jpa.SuperheroesApplicationPostgeSQLContainer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -16,19 +17,37 @@ import de.claudioaltamura.springboot.jpa.superheroes.repository.SuperheroEntityR
 @DataJpaTest
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class SuperheroEntityDataJpaTest extends SuperheroesApplicationPostgeSQLContainer {
+class SuperheroEntityRepositoryTest extends SuperheroesApplicationPostgeSQLContainer {
 
 	@Autowired
 	private SuperheroEntityRepository superheroEntityRepository;
 
-	@Test
-	void testInteractionWithDatabase() {
+	@BeforeEach
+	public void setUp() {
 		final var city = new CityEntity("New York City");
 		superheroEntityRepository.save(new SuperheroEntity("Spider-Men", "Peter Parker", 92.0d, city));
-
+	}
+	@Test
+	void findByNameShouldReturnSuperhero() {
 		List<SuperheroEntity> superheroes = superheroEntityRepository.findByName("Spider-Men");
 
 		assertThat(superheroes).hasSize(1);
 		assertThat(superheroes.get(0).getCity().getName()).isEqualTo("New York City");
+	}
+
+	@Test
+	void findByNameStartsWithShouldReturnSuperhero() {
+		List<SuperheroEntity> superheroes = superheroEntityRepository.findByNameStartsWith("Sp");
+
+		assertThat(superheroes).hasSize(1);
+		assertThat(superheroes.get(0).getName()).isEqualTo("Spider-Men");
+	}
+
+	@Test
+	void searchContainsByNameLikeShouldReturnSuperhero() {
+		List<SuperheroEntity> superheroes = superheroEntityRepository.searchContainsByNameLike("der");
+
+		assertThat(superheroes).hasSize(1);
+		assertThat(superheroes.get(0).getName()).isEqualTo("Spider-Men");
 	}
 }
