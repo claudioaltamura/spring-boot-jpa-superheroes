197b340fe6df6a0e09fa0b0744c5e4976930ae3e
Claudio Altamura <claudio@claudioaltamura.de>
2024-02-09 11:51:14 +0100

    test: using sql based fixture

--- src/test/java/de/claudioaltamura/springboot/jpa/superheroes/entity/SuperheroEntityRepositoryTest.java
+++ src/test/java/de/claudioaltamura/springboot/jpa/superheroes/entity/SuperheroEntityRepositoryTest.java
@@ -5,44 +5,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import de.claudioaltamura.springboot.jpa.SuperheroesApplicationPostgeSQLContainer;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import de.claudioaltamura.springboot.jpa.superheroes.repository.SuperheroEntityRepository;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@Sql("/fixtures/insert-cities-superheroes.sql")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SuperheroEntityRepositoryTest extends SuperheroesApplicationPostgeSQLContainer {
 
 	@Autowired
 	private SuperheroEntityRepository superheroEntityRepository;
 
-	@BeforeEach
-	public void setUp() {
-		final var city = new CityEntity("New York City");
-		superheroEntityRepository.save(new SuperheroEntity("Spider-Men", "Peter Parker", 92.0d, city));
-		superheroEntityRepository.save(new SuperheroEntity("Iron Man", "Tony Stark", 99.0d, new CityEntity("Malibu")));
-	}
-
 	@Test
-	void findAll() {
-		List<SuperheroEntity> all = superheroEntityRepository.findAll();
-
-		assertThat(all).hasSize(3);
-	}
-	@Test
-	void findByNameShouldReturnSuperhero() {
+	void shouldReturnSuperheroWhenFindByName() {
 		List<SuperheroEntity> superheroes = superheroEntityRepository.findByName("Spider-Men");
 
 		assertThat(superheroes).hasSize(1);
-		assertThat(superheroes.get(0).getCity().getName()).isEqualTo("New York City");
+		assertThat(superheroes.get(0).getCity().getName()).isEqualTo("NYC");
 	}
 
 	@Test
-	void findByNameStartsWithShouldReturnSuperhero() {
+	void shouldReturnSuperheroWhenFindByNameStartWith() {
 		List<SuperheroEntity> superheroes = superheroEntityRepository.findByNameStartsWith("Sp");
 
 		assertThat(superheroes).hasSize(1);
@@ -50,7 +38,7 @@ class SuperheroEntityRepositoryTest extends SuperheroesApplicationPostgeSQLConta
 	}
 
 	@Test
-	void searchContainsByNameLikeShouldReturnSuperhero() {
+	void shouldReturnSuperheroWhenSearchContainsByNameLike() {
 		List<SuperheroEntity> superheroes = superheroEntityRepository.searchContainsByNameLike("der");
 
 		assertThat(superheroes).hasSize(1);
---
+++ src/test/resources/fixtures/insert-cities-superheroes.sql
@@ -0,0 +1,5 @@
+INSERT INTO cities(id, "name") VALUES(2, 'NYC');
+INSERT INTO cities(id, "name") VALUES(3, 'MALIBU');
+
+INSERT INTO superheroes (id, "name", real_name, power, city_id) VALUES(2, 'Spider-Men', 'Peter Parker', 92.0, 2);
+INSERT INTO superheroes (id, "name", real_name, power, city_id) VALUES(3, 'Iron Man', 'Tony Stark', 99.0, 3);
